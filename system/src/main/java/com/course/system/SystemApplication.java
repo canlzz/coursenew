b2bc781fb8c62229c5e69a4a8d6204b4e3a0eaf9
liangzhouzhou <2502674949@qq.com>
2020-12-01 04:37:26 +0800

    1. system注册到eureka中

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 /**
@@ -19,7 +20,7 @@ import org.springframework.core.env.Environment;
  *  
  */
 @SpringBootApplication
-
+@EnableEurekaClient
 public class SystemApplication {
     private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
 
