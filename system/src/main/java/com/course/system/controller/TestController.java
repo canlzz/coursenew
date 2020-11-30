ddcabc2a72fcd511b55aed985c1c8f6f3909531f
liangzhouzhou <2502674949@qq.com>
2020-12-01 04:28:15 +0800

    1. system测试controller

---
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.course.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *  
+ *  * @projectName course
+ *  * @title     TestController   
+ *  * @package    com.course.controller  
+ *  * @description      
+ *  * @author juzizhou     
+ *  * @date   2020/11/30 20:36  
+ *  * @version V1.0.0
+ *  * @copyright 2020 www.XXXXX.com
+ *  
+ */
+@RestController
+public class TestController {
+    @RequestMapping("test")
+    public String test(){
+        return "success";
+    }
+}
