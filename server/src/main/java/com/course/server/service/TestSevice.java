bda239c2f804126989380588ca893929dd93118a
liangzhouzhou <2502674949@qq.com>
2020-12-01 21:03:23 +0800

    1.增加server模块

---
+++ server/src/main/java/com/course/server/service/TestSevice.java
@@ -0,0 +1,32 @@
+package com.course.server.service;
+
+import com.course.server.domain.Test;
+import com.course.server.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ *  
+ *  * @projectName course
+ *  * @title     TestMapper   
+ *  * @package    com.course.system.mapper  
+ *  * @description      
+ *  * @author juzizhou     
+ *  * @date   2020/12/1 6:23  
+ *  * @version V1.0.0
+ *  * @copyright 2020 www.XXXXX.com
+ *  
+ */
+@Service
+public class TestSevice {
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list() {
+        return testMapper.list();
+    }
+
+
+}
